afd8964d18d2260afcf2b45740e651e11592b3f5
Jason Britto <35361130+john9088@users.noreply.github.com>
2021-02-01 23:20:43 +0000

    Final Commit

--- app/src/androidTest/java/com/jasonbritto/simplyinfopad/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/jasonbritto/simplyinfopad/ExampleInstrumentedTest.kt
@@ -19,6 +19,7 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.gridlayouttut", appContext.packageName)
+        println("***"+appContext.packageName)
+        assertEquals("com.jasonbritto.simplyinfopad", appContext.packageName)
     }
 }
